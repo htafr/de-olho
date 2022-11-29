6c356530f44e784a142bfd786f3606c128760324
offreitas <offreitas@outlook.com>
2022-11-29 18:17:31 -0300

    WIP: Proposicao page

--- proposicoes/views.py
+++ proposicoes/views.py
@@ -1,41 +1,43 @@
 from django.shortcuts import render
+from datetime import datetime
 import requests
 import pandas as pd
 import json
 
 # Create your views here.
 
-def index(request):
-    """View function for home page of site."""
+def RenderProposicoes(request, year=datetime.now().year):
+    """
+        View function for home page of Proposições.
+    """
 
-
-    response = requests.get("https://dadosabertos.camara.leg.br/arquivos/proposicoes/json/proposicoes-2022.json")
-    response = response.json()["dados"]
-    proposicoes_df = pd.json_normalize(response)
-    data = proposicoes_df.to_json()
-    data = json.loads(data)
-
-    # # Generate counts of some of the main objects
-    # num_books = Book.objects.all().count()
-    # num_instances = BookInstance.objects.all().count()
-
-    # # Available books (status = 'a')
-    # num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-
-    # # The 'all()' is implied by default.
-    # num_authors = Author.objects.count()
-
-    # context = {
-    #     'num_books': num_books,
-    #     'num_instances': num_instances,
-    #     'num_instances_available': num_instances_available,
-    #     'num_authors': num_authors,
-    # }
-
+    data = GetProposicoes(year)
     context = {
-        "data" : proposicoes_df,
-        "range" : range(len(proposicoes_df)),
+        "data" : data,
+        "years" : range(datetime.now().year, 1984, -1),
     }
 
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    if request.method == "POST":
+        year = request.POST.get("select_year")
+        data = GetProposicoes(year)
+        context["data"] = data
+        context["data_len"] = range(len(data))
+        context["year"] = year
+        return render(request, "proposicoes.html", context=context)
+    else:
+        return render(request, "proposicoes.html", context=context)
+
+def GetProposicoes(year):
+    """
+        Get data from Dados Abertos Câmara dos Deputados from selected year
+    """
+    url = f"http://dadosabertos.camara.leg.br/arquivos/proposicoes/json/proposicoes-{year}.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        df = pd.json_normalize(response.json()["dados"])
+        df = df[["id", "uri", "siglaTipo", "ementa", "descricaoTipo"]]
+        df = df.assign(Tipo = df.descricaoTipo.astype(str) + " (" + df.siglaTipo + ")")
+        df = df.drop(columns=["siglaTipo", "descricaoTipo"])
+        return df
+    else:
+        return response.raise_for_status()
